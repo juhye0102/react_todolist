f2bed0da4e1f6304ff84a063f023086fde523408
ohjuhye <bbangbbang562@gmail.com>
2022-06-14 23:45:39 +0900

    [add]

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+//rsc
+
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  return <HeaderText>할 일</HeaderText>;
+};
+
+const HeaderText = styled.h1`
+  color: red;
+`;
+
+export default Header;
