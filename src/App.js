fe6715349ef500136e0d74a124bfd699cb11958e
ohjuhye <bbangbbang562@gmail.com>
2022-06-14 23:45:19 +0900

    [modify]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import Header from "./components/header/Header";
+import Input from "./components/input/Input";
+import List from "./components/list/List";
 
 function App() {
+  // getter, setter 느낌 ~~
+  const [todolist, setTodolist] = useState([]);
+  const [text, setText] = useState("");
+
+  // 컴포넌트 생명을 관리하는 역할
+  useEffect(() => {
+    console.log(todolist);
+  }, [todolist]); // 언제 바꿀건지?! todolsit의 값이 변했을때 console 찍어라~
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodolistWrapper>
+        <Header />
+        <Input
+          todolist={todolist}
+          setTodolist={setTodolist}
+          text={text}
+          setText={setText}
+        />
+        <List todolist={todolist} />
+      </TodolistWrapper>
+    </>
   );
 }
+
+const TodolistWrapper = styled.section`
+  width: 500px;
+  height: 700px;
+  background-color: #f4f4f4;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default App;
